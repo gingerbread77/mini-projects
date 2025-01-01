6cb482f9f54cd54b7eee97e00320ddcc12dc0ee9
gingerbread77 <1516519317@qq.com>
2025-01-01 15:13:11 +1300

    Add hidden search widget

---
+++ hidden-search/script.js
@@ -0,0 +1,4 @@
+const search = document.querySelector('.search-container');
+const searchBtn = document.querySelector('button');
+
+searchBtn.addEventListener('click',()=>search.classList.toggle('active'));
