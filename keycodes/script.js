50634a6aad426ec7d0a23c14ac056b8885ad866f
gingerbread77 <1516519317@qq.com>
2025-01-04 21:08:26 +1300

    Implement key code display on keydown event

---
+++ keycodes/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+
+function getKeyData(e) {
+  return [
+    { label: "event.key", value: e.key === " " ? "Space" : e.key },
+    { label: "event.keyCode", value: e.keyCode },
+    { label: "event.code", value: e.code },
+  ];
+}
+
+// display the information of the key pressed
+function updatePage(keyData) {
+  keyData.forEach((data) => {
+    const key = document.createElement("div");
+    const text = document.createElement("p");
+    const box = document.createElement("div");
+
+    key.classList.add("key");
+    box.classList.add("box");
+
+    text.innerHTML = data.label;
+    box.innerHTML = data.value;
+    key.appendChild(text);
+    key.appendChild(box);
+    container.appendChild(key);
+  });
+}
+
+window.addEventListener("keydown", (e) => {
+  const keyData = getKeyData(e);
+  // clear the container
+  container.innerHTML = "";
+  updatePage(keyData);
+});
